ac6da64d56116f127eb27a18dca90441a27c7918
cjsakshi29 <cjsakshi29@gmail.com>
2026-02-15 19:59:38 +0530

    Update import path for Tool from langchain_core

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import requests
 import base64
 
-from langchain.tools import Tool
+from langchain_core.tools import Tool
 from langchain.agents import initialize_agent, AgentType
 from langchain_community.chat_models import ChatOpenAI
 
